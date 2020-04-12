18d88d8a57a38655265c926030fce4fb8ffb7e60
Erro Remaider Ashivudhi <pandenierro@gmail.com>
2020-04-12 17:41:02 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,6 @@
 # Use this space to code along with the readme
-
+def phrase_method
 phrase = "Hello World!"
 puts phrase
 puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+end
